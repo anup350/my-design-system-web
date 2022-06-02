df80342707c561263e8d78513b7c3daabeeadf3a
Anup Salunke <anupsalunke@094AMBBLR-Shanky-Munoth-Prakash.local>
2022-06-02 19:02:59 +0530

    Cleanup branch

--- src/components/types.ts
+++
@@ -1,8 +0,0 @@
-export type ButtonAppearance = 'primary' | 'secondary' | 'danger' | 'success' | 'warning';
-export type ButtonSize = 'small' | 'medium' | 'large';
-export type Targets = '_blank' | '_self' | '_parent' | '_top';
-
-export const TagTypes = {
-	button: 'button',
-	a: 'a',
-};
